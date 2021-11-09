746cb356e7a4d1c22184a43f8a6fed5930b5e0ff
fenghan <fenghan770@gmail.com>
2021-11-09 17:31:46 +0800

    chore: integrate tailwindcss

--- next.config.js
+++ next.config.js
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  // experimental: { esmExternals: true },
+  // pageExtensions: ['md', 'mdx', 'tsx', 'ts'],
+  // webpack(config, options) {
+  //   config.module.rules.push({
+  //     test: /\.mdx?$/,
+  //     use: [
+  //       options.defaultLoaders.babel,
+  //       {
+  //         loader: '@mdx-js/loader',
+  //         /** @type {import('@mdx-js/loader').Options} */
+  //         options: {},
+  //       },
+  //     ],
+  //   })
+
+  //   return config
+  // },
 }

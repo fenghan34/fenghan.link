311dcda9bd4bbd854055033c5bc2b5c94bb12894
fenghan <fenghan770@gmail.com>
2021-11-10 18:00:16 +0800

    chore: use mdx

--- next.config.js
+++ next.config.js
@@ -1,21 +1,21 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  // experimental: { esmExternals: true },
-  // pageExtensions: ['md', 'mdx', 'tsx', 'ts'],
-  // webpack(config, options) {
-  //   config.module.rules.push({
-  //     test: /\.mdx?$/,
-  //     use: [
-  //       options.defaultLoaders.babel,
-  //       {
-  //         loader: '@mdx-js/loader',
-  //         /** @type {import('@mdx-js/loader').Options} */
-  //         options: {},
-  //       },
-  //     ],
-  //   })
+  experimental: { esmExternals: true },
+  pageExtensions: ['md', 'mdx', 'tsx', 'ts'],
+  webpack(config, options) {
+    config.module.rules.push({
+      test: /\.mdx?$/,
+      use: [
+        options.defaultLoaders.babel,
+        {
+          loader: '@mdx-js/loader',
+          /** @type {import('@mdx-js/loader').Options} */
+          options: {},
+        },
+      ],
+    })
 
-  //   return config
-  // },
+    return config
+  },
 }
